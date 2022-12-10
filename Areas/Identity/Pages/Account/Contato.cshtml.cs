ab52093153b669a8dc7e23d92841a5b36e5764bf
abrah <abrahamluccas13@gmail.com>
2022-12-10 13:14:04 -0300

    contato n puxa foto

---
+++ Areas/Identity/Pages/Account/Contato.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmpregaSENAI.Areas.Identity.Pages.Account
+{
+    public class ContatoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
